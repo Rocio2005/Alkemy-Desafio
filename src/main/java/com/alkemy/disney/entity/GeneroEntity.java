277058dd514e8727ac2e4954ae3847acbb676892
Rocio2005 <rochi1605@gmail.com>
2022-01-11 20:46:39 -0300

    agrego las entidades del proyecto desafío de Alkemy

---
+++ src/main/java/com/alkemy/disney/entity/GeneroEntity.java
@@ -0,0 +1,14 @@
+package com.alkemy.disney.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "genero")
+@Getter
+@Setter
+public class GeneroEntity {
+}
