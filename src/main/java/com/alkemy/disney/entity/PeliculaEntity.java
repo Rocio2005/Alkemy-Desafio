404aa6f95d3a7efef5edc1f6739ff85a3507fe9f
Rocio2005 <rochi1605@gmail.com>
2022-01-13 21:59:59 -0300

    agrego las relaciones entre las entidades Pelicula-Personaje y Pelicula-Genero

--- src/main/java/com/alkemy/disney/entity/PeliculaEntity.java
+++ src/main/java/com/alkemy/disney/entity/PeliculaEntity.java
@@ -2,13 +2,58 @@ package com.alkemy.disney.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name= "pelicula")
 @Getter
 @Setter
 public class PeliculaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String imagen;
+
+    private String titulo;
+
+    @Column(name="fecha_creacion")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate fechaCreacion;
+
+    private Short calificacion;
+
+    /*relacion pelicula-genero*/ /*muchas peliculas para un genero, o una pelicula un genero y un genero muchas peliculas*/
+    @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinColumn(name="genero_id", insertable=false,updatable = false)/*no puedo insertar ni actualizar pues es solo para obtener informacion*/
+    private GeneroEntity genero;
+
+    @Column(name="genero_id",nullable = false)
+    private Long generoId;
+
+    /*relacion pelicula-personaje*/
+    @ManyToMany(
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            })
+    @JoinTable(
+            name = "personaje_pelicula",
+            joinColumns = @JoinColumn(name = "pelicula_id"),/*donde estoy parada, en este caso pais*/
+            inverseJoinColumns = @JoinColumn(name = "personaje_id"))/*la relacion con la otra columna en otro lado, en este caso icon*/
+    private Set<PersonajeEntity> personajes= new HashSet<>();
+
+
+
+
+
+
 }
+
+
--- src/main/java/com/alkemy/disney/entity/PersonajeEntity.java
+++ src/main/java/com/alkemy/disney/entity/PersonajeEntity.java
@@ -3,12 +3,29 @@ package com.alkemy.disney.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name= "personaje")
 @Getter
 @Setter
 public class PersonajeEntity {
+
+
+
+
+
+
+    @ManyToMany(mappedBy = "personajes",cascade = CascadeType.ALL)
+    private List<PeliculaEntity> peliculas=new ArrayList<>();
+
+    //add and remove paises
+    public void addPais(PeliculaEntity pelicula) { this.peliculas.add(pelicula);}
+
+    public void removePais(PeliculaEntity pelicula){ this.peliculas.remove(pelicula);}
 }
